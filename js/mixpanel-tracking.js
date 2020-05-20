68f289c4013c72bf0ba0521decccb1a17a729392
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2020-05-20 15:51:44 +0300

    tracking also index + initial page

--- js/mixpanel-tracking.js
+++ js/mixpanel-tracking.js
@@ -1,31 +1,50 @@
-/**
- * initializing Mixpanel and sending standard page view
-*/
+(function() {
+  mixpanel.init("99035923ee0a67880e6c05ab92b6cbc0", { batch_requests: true })
+  mixpanel.track('page view', {
+  'page title': document.title,
+  'page type': 'slideshow'
+  });
 
-mixpanel.init("99035923ee0a67880e6c05ab92b6cbc0", { batch_requests: true })
-mixpanel.track('page view', {
-'page title': document.title,
-'page type': 'slideshow'
-});
-
-
-window.addEventListener('load', (event) => {
-  function trackSlideViewEvent(pageId, pageTitle) {
-    mixpanel.track('slideshow page view', {
-      'page id': pageId,
-      'page title': pageTitle,
-    })
-  }
-
-  Reveal.addEventListener( 'slidechanged', event => {
+  function extractDataFromEvent(event) {
     const id = event.currentSlide.id;
+    const index = event.indexh;
     let title;
     try {
       title = event.currentSlide.dataset.menuTitle;
-    } catch (error) {
+    }
+    catch (error) {
       console.error(error);
       title = '';
     }
-    trackSlideViewEvent(id, title);
-  } );
-});
+    return {
+      'id': id,
+      'title': title,
+      'index': index
+    };
+  }
+
+  /**
+   *
+   * @param eventData {{id: string, title: string, index: number}}
+   */
+  function trackSlideViewEvent(eventData) {
+    mixpanel.track('slideshow page view', {
+      'page id': eventData.id,
+      'page title': eventData.title,
+      'page index': eventData.index
+    })
+  }
+
+  function addRevealEventListener(eventName) {
+    Reveal.addEventListener(eventName, event => {
+      var eventData = extractDataFromEvent(event);
+      trackSlideViewEvent(eventData);
+    } );
+  }
+
+  addRevealEventListener('ready');
+  addRevealEventListener('slidechanged');
+}());
+/**
+ * initializing Mixpanel and sending standard page view
+*/
