0a13e50418717de0f513c1264a0e2836559e5c9d
alexandru-m-g <alexandru-m-g@users.noreply.github.com>
2020-08-21 16:57:37 +0300

    Switching to using the MP production project

--- js/mixpanel-tracking.js
+++ js/mixpanel-tracking.js
@@ -1,5 +1,5 @@
 (function() {
-  mixpanel.init("99035923ee0a67880e6c05ab92b6cbc0", { batch_requests: true })
+  mixpanel.init("5cbf12bc9984628fb2c55a49daf32e74", { batch_requests: true })
   mixpanel.track('page view', {
   'page title': document.title,
   'page type': 'slideshow'
